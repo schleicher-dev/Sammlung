65a05bea5378ab817e1cec2142cb39cb732b8180
Ralf Schleicher <mail@ralfschleicher.de>
2021-01-06 16:30:16 +0100

    Added LockFreeLinkedDeque

--- Sammlung/Queues/Concurrent/LockFreePrimitives/Anchor.cs
+++ Sammlung/Queues/Concurrent/LockFreePrimitives/Anchor.cs
@@ -1,14 +1,32 @@
 using System;
 using System.Threading;
+using Sammlung.Utilities.Patterns;
 
 namespace Sammlung.Queues.Concurrent.LockFreePrimitives
 {
+    internal sealed class AnchorObjectPool<T> : ObjectPoolBase<Anchor<T>>
+    {
+        /// <inheritdoc />
+        protected override Anchor<T> CreateInstance() => Anchor<T>.Create();
+
+        /// <inheritdoc />
+        protected override Anchor<T> ResetInstance(Anchor<T> instance)
+        {
+            instance.LeftMost = default;
+            instance.RightMost = default;
+            instance.State = default;
+            instance.ReferenceCount = default;
+            return instance;
+        }
+    }
+    
     internal sealed class Anchor<T>
     {
-        public static Anchor<T> Create() => new Anchor<T>();
+        public static Anchor<T> Create() => new Anchor<T> { ReferenceCount = 0 };
         public static Anchor<T> Create(Node<T> left, Node<T> right, State state) => 
-            new Anchor<T>{ LeftMost = left, RightMost = right, State = state};
+            new Anchor<T>{ LeftMost = left, RightMost = right, State = state, ReferenceCount = 0};
 
+        public int ReferenceCount;
         public Node<T> LeftMost;
         public Node<T> RightMost;
         public State State;
