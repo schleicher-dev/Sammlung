eb18a50e1a7571030193eeb334283815c99297a5
Ralf Schleicher <mail@ralfschleicher.de>
2021-11-24 23:43:44 +0100

    Added new numerics package with an Bound.cs and Interval.cs classes.

---
+++ Sammlung.Numerics/ParamValidationExtensions.cs
@@ -0,0 +1,31 @@
+using System;
+using Sammlung.Numerics.Resources;
+using Sammlung.Werkzeug;
+
+namespace Sammlung.Numerics
+{
+    /// <summary>
+    /// The <see cref="ParamValidationExtensions"/> type extends types with helpful validation checks.
+    /// </summary>
+    [JetBrains.Annotations.PublicAPI]
+    public static class ParamValidationExtensions
+    {
+        /// <summary>
+        /// Requires the value to be an element of the given interval.
+        /// </summary>
+        /// <param name="value">the value</param>
+        /// <param name="interval">the interval</param>
+        /// <param name="paramName">the parameter name</param>
+        /// <typeparam name="T">the number type</typeparam>
+        /// <returns>the value if element of the interval</returns>
+        /// <exception cref="ArgumentOutOfRangeException">when the value is not element of the interval</exception>
+        public static T RequireElementOf<T>(this T value, Interval<T> interval, string paramName)
+            where T : IComparable<T>
+        {
+            return interval.RequireNotNull(nameof(interval)).Contains(value)
+                ? value
+                : throw new ArgumentOutOfRangeException(paramName, value,
+                    string.Format(ErrorMessages.ParamRequireElementOf, interval));
+        }
+    }
+}
