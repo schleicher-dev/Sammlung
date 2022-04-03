feb6a9e9fb712984db5f6bf4657a55dce46195d3
Ralf Schleicher <mail@ralfschleicher.de>
2022-04-03 21:31:33 +0200

    Fixed errors in BinaryHeap.cs

--- Sammlung.Collections.Heaps/BinaryHeap.cs
+++ Sammlung.Collections.Heaps/BinaryHeap.cs
@@ -2,8 +2,8 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Sammlung.DesignPatterns;
 using Sammlung.Werkzeug;
-using Sammlung.Werkzeug.Patterns;
 
 namespace Sammlung.Collections.Heaps
 {
