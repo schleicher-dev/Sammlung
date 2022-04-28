7b732fb3c37688dc6d68daded49f8e7a0ae48944
Ella Mareis <ella.mareis@zeiss.com>
2022-04-28 17:34:06 +0200

    improve exception for string null or empty

--- Sammlung.Werkzeug/Resources/ErrorMessages.Designer.cs
+++ Sammlung.Werkzeug/Resources/ErrorMessages.Designer.cs
@@ -19,7 +19,7 @@ namespace Sammlung.Werkzeug.Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class ErrorMessages {
@@ -142,7 +142,7 @@ namespace Sammlung.Werkzeug.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to The string parameter is expected to be not null or empty..
+        ///   Looks up a localized string similar to The string parameter {0} is expected to be not null or empty..
         /// </summary>
         internal static string ParamStringRequiredNotNullOrEmpty {
             get {
